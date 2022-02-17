a303d505249fef1dfa3a9acbfb38c6b5d9ebb3e3
kostyaTsy <tsivilko02@mail.ru>
2022-02-17 17:58:54 +0300

    Added to lab1V1 examination of arguments

--- lab1/lab1V1.sh
+++ lab1/lab1V1.sh
@@ -3,6 +3,33 @@
 if [ $# -ne 3 ]
 then
 	echo "Number of params should be 3">&2
+	echo "Command format: min size, max size, directory"
+	exit 1
+fi
+
+# checking for correct dir
+if [ ! -d "$3" ]
+then
+	echo "Incorect argument 3, not the direcrory">&2
+	exit 1
+fi 
+
+# cheking for a number
+if (echo "$1" | grep -E -q "[^0-9]+$")
+then
+	echo "First argument is not a number">&2
+	exit 1
+fi
+
+if (echo "$2" | grep -E -q "[^0-9]+$")
+then
+	echo "Second argument is not a number">&2
+	exit 1
+fi
+
+if [ "$1" -ge "$2" ]
+then
+	echo "First param should be less than second">&2
 	exit 1
 fi
 
